508dfcf4a55a1ac45eed0f1993c74857ad2205b2
tyz01 <jenaynogach1@mail.ru>
2023-12-27 16:23:30 +0300

    JDBC Starter

--- src/main/java/org/example/spring/database/repository/FilterUserRepository.java
+++ src/main/java/org/example/spring/database/repository/FilterUserRepository.java
@@ -1,6 +1,8 @@
 package org.example.spring.database.repository;
 
+import org.example.spring.dto.PersonalInfo;
 import org.example.spring.dto.UserFilter;
+import org.example.spring.entity.Role;
 import org.example.spring.entity.User;
 
 import java.util.List;
@@ -8,4 +10,9 @@ import java.util.List;
 public interface FilterUserRepository {
     List<User> findAllByFilter(UserFilter userFilter);
 
+    List<PersonalInfo> findAllByCompanyIdAndRole(Long companyId, Role role);
+
+    void updateCompanyAndRole(List<User> users);
+
+    void updateCompanyAndRoleNamed(List<User> users);
 }
--- src/main/java/org/example/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/org/example/spring/database/repository/FilterUserRepositoryImpl.java
@@ -3,20 +3,55 @@ package org.example.spring.database.repository;
 import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
 import org.example.spring.database.querydsl.QPredicates;
+import org.example.spring.dto.PersonalInfo;
 import org.example.spring.dto.UserFilter;
 import org.example.spring.entity.QUser;
+import org.example.spring.entity.Role;
 import org.example.spring.entity.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.Predicate;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.example.spring.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
 
+    private static final String FIND_BY_COMPANY_AND_ROLE = """
+                   
+            SELECT 
+                firstname, 
+                lastname, 
+                birth_date
+            FROM users
+            WHERE company_id = ?
+            AND role = ?
+            """;
+
+    private static final String UPDATE_COMPANY_AND_ROLE = """
+            UPDATE users
+            SET company_id = ?,
+                role = ?
+            WHERE id = ?     
+            """;
+
+    private static final String UPDATE_COMPANY_AND_ROLE_NAMED = """
+            UPDATE users
+            SET company_id = :company_id,
+                role = role
+            WHERE id = :id     
+            """;
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final EntityManager entityManager;
 
 //    @Override
@@ -50,5 +85,38 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
                 .from(user)
                 .where(predicate)
                 .fetch();
+    }
+
+    @Override
+    public List<PersonalInfo> findAllByCompanyIdAndRole(Long companyId, Role role) {
+        return jdbcTemplate.query(FIND_BY_COMPANY_AND_ROLE, (rs, rowNum) -> new PersonalInfo(
+                rs.getString("firstname"),
+                rs.getString("lastname"),
+                rs.getDate("birth_date").toLocalDate()
+        ), companyId, role.name());
+    }
+
+    @Override
+    public void updateCompanyAndRole(List<User> users) {
+        var args = users.stream()
+                .map(user -> new Object[]{
+                        user.getCompany().getId(),
+                        user.getRole().name(),
+                        user.getId()
+                }).toList();
+        jdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE, args);
+    }
+
+    @Override
+    public void updateCompanyAndRoleNamed(List<User> users) {
+        var args = users.stream()
+                .map(user -> Map.of(
+                        "companyId", user.getCompany().getId(),
+                        "role", user.getRole().name(),
+                        "id", user.getId()
+                ))
+                .map(MapSqlParameterSource::new)
+                .toArray(MapSqlParameterSource[]::new);
+        namedParameterJdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE_NAMED, args);
     }
 }
